c1f6e361c2cbc47d9affc99f66097861500647de
Joshim Uddin <98439021+Joshim-Uddin@users.noreply.github.com>
2022-12-26 21:07:00 +0600

    this first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react';
+
+import Users from './components/Users';
+
+const App = () => {
+  // step1 : declare three states here : users, isLoading, error
+  const [users, setUsers] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState(null)
+
+  // step2 : use useEffect for fetching the data including updating isLoading and error states
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((res)=>{
+      if(!res.ok){
+        throw Error('Fetching is not successful')
+      }
+      return res.json()
+    })
+    .then((data)=>{
+      setUsers(data)
+      setIsLoading(false)
+    })
+    .catch((error)=>{
+      setError(error.message)
+    })
+  })
+
+  return (
+    <div className="container">
+      <h1 className="title">Users Management App</h1>
+      {isLoading && <p>Loading users...</p>}
+      {error && <p>{error}</p>}
+      <Users users= {users}/>
+    </div>
+  );
+};
+
+export default App;
